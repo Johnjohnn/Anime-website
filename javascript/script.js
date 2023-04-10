5b3564175903583de861681e55953cd2dcb6e8db
johnjohnn <conde2010@aol.com>
2023-04-09 23:49:27 -0400

    saving before I leave

--- javascript/script.js
+++ javascript/script.js
@@ -13,7 +13,7 @@ function showSlides() {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";  
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
